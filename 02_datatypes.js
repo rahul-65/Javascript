8dc15d8c0f58cb878c1d7fcc26f03a9418a7750a
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-04-22 07:31:50 +0000

    changes

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+// we are  using node js not an browser
+
+console.log("rahul");
+
+//number=> numeric
+//bigint=> big number
+//string => ""
+//Boolean =>true/false
+//null =>standalone value
+//undefined =not defined
+//symbol =>unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 03_conversion_oper.js
@@ -0,0 +1,67 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+ //"33" ="33"
+// "33abc" = NaA //not an number
+//true =>1; false =>0
+
+let isLoggedIn ="rahul"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+//1 = true ; 0 =false
+//"" = false 
+//"rahul"= true
+
+let someNumber =65
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  *******  OPERATIONS   *********
+
+let value = 3
+let negValue=-value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 = "rahul"
+
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+(2+"2"));
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 , num2 , num3= 2 + 2
+
+//  let gameCounter = 100
+//  gameCounter++;
+//  console.log(gameCounter);
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
