cd846b07f1a5e8def90f49d696c590360116dab8
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-06-20 09:34:14 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,6 @@
+
+//code readability is high
+
 // we are  using node js not an browser
 
 console.log("rahul");
@@ -11,6 +14,11 @@ console.log("rahul");
 //symbol =>unique
 
 //object
+let num =25
+let isLoggedIn =false
+let bigint=12455n
 
-console.log(typeof undefined);
-console.log(typeof null);
+let hi ="rahulya"
+
+console.log(typeof undefined,num ,isLoggedIn,bigint,hi);
+console.log(typeof null,num ,bigint,String);
