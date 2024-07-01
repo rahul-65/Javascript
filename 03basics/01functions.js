8e4c96cf1b4f787acd018362f6510335883da18f
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-07-01 09:47:49 +0000

    functions

--- 03basics/01functions.js
+++ 03basics/01functions.js
@@ -22,4 +22,28 @@ return `${username} just logged in`
 function calculateCartPrice(num1){
     return num1
 }
-console.log(calculateCartPrice(2));
+//console.log(calculateCartPrice(2));
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+const user ={
+    username:"rahul",
+    price:199
+}
+function handleObject(anyobject){
+   console.log(`username is ${anyobject.username}and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username:"sunny",
+    price:299
+})
+
+const myNewArray = [200 , 300 , 400 , 500]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 , 500 , 400 , 507]));
