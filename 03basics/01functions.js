d3f084ec0cb51a7b9e4193e665436ef72858c125
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-07-01 09:05:36 +0000

    functions

---
+++ 03basics/01functions.js
@@ -0,0 +1,20 @@
+//function
+function sayMyName(){
+console.log("R");
+console.log("A");
+console.log("H");
+console.log("U");
+console.log("L");
+}
+
+//sayMyName
+
+function addTwoNumbers(number1 , number2){
+    console.log(number1 + number2);
+}
+//addTwoNumbers( 4 , 4)
+
+function loginUserMessage (username){
+return `${username} just logged in`
+}
+console.log(loginUserMessage("rahul"))
