fa8d4768820c6fe9f2f670c4b01e690aab4a47d1
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-07-03 08:36:54 +0000

    scope

---
+++ 03basics/02scope.js
@@ -0,0 +1,23 @@
+
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username ="rahul"
+    
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+
+}
+one()
