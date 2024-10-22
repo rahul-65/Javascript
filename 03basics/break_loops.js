4f15e32f87002727137dd0782a3df45bfdd80379
Rahul Lokhande <167507478+rahul-65@users.noreply.github.com>
2024-10-22 12:47:07 +0000

    loops

---
+++ 03basics/break_loops.js
@@ -0,0 +1,60 @@
+//  for (i=1; i<=; i++){
+//      if(i==4) {
+//          break;
+//      }
+//      else {
+//          console.log(i);
+        
+//      }
+//  }
+
+// for (i=1; i<=4; i++){
+//     if(i==2) {
+//         continue;
+//     }
+//     else {
+//         console.log(i);
+        
+//     }
+// }
+
+// for (i =1; i<=12; i++)
+// {
+//     if (i ==4 || i==8 || i==10)
+//     {
+//         console.log("Gayatri");
+        
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+        
+//     }
+// }
+
+// for (i=1;  i<=5; i++)
+// {
+//     console.log(i);
+// }
+//     for (j=11; j<=15; j++ )
+//         {
+//             console.log(j);
+//         }
+//             for (k=21; k<=25; k++ )
+//             {
+//                 console.log(k);
+//             }
+         
+
+    for(i=1; i<=30; i++)
+    {
+        if((i>=6 && i<=10) || (i>=16 && i<=20) || (i>=26 && i<=30) )
+        {
+            continue;
+        }
+        else
+        {
+            console.log(i);
+        }
+    
+    }
--- 03basics/for_loops.js
+++ 03basics/for_loops.js
@@ -1,4 +1,14 @@
-for (i=5 ; i>0 ; i--);
+//counting number
+
+//for (i=1 ; i<10; i++)
+//{
+//    console.log(i);
+// }
+
+//reverse counting
+
+for(i= 5; i>0; i--)
 {
     console.log(i);
+    
 }
